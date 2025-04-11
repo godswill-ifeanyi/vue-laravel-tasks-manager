e0643a10c63aeaaa90af99be02f55b8a7f648b4d
Godswill Ifeanyi Okpanku <119576849+godswill-ifeanyi@users.noreply.github.com>
2025-04-11 11:24:28 +0100

    Initial Commit

---
+++ server/storage/framework/views/159d1c200aa5a3817baadad5b330f8b8.php
@@ -0,0 +1,4 @@
+<?php if($authenticated): ?><?php $__env->startComponent('scribe::components.badges.base', ['colour' => "darkred", 'text' => 'requires authentication']); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php /**PATH C:\xampp1\htdocs\laravel\crud_api\server\vendor\knuckleswtf\scribe\src/../resources/views//components/badges/auth.blade.php ENDPATH**/ ?>
---
+++ server/storage/framework/views/24ec1f31c81a894ee8977fc151196a5c.php
@@ -0,0 +1,2 @@
+<small class="badge badge-<?php echo e($colour); ?>"><?php echo e($text); ?></small>
+<?php /**PATH C:\xampp1\htdocs\laravel\crud_api\server\vendor\knuckleswtf\scribe\src/../resources/views//components/badges/base.blade.php ENDPATH**/ ?>
